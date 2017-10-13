12012faacb3793a58584fe8124cbcc7e898444cc
Maciek Bajon <maciej.bajon@gft.com>
2017-10-13 17:12:24 +0200

    xx

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,7 +1,9 @@
 import { Ingredient } from '../common/ingredient.model'
+import { EventEmitter } from '@angular/core'
 
 export class ShoppingListService {
    private  ingredients: Ingredient[] = [new Ingredient("Apple", 5), new Ingredient("Banana", 5)]
+   ingredientsChanged = new EventEmitter<Ingredient[]>()
 
    getIngredients(){
        return this.ingredients.slice();
@@ -9,6 +11,7 @@ export class ShoppingListService {
 
    addIngredient(ingredient : Ingredient){
        this.ingredients.push(ingredient)
+       this.ingredientsChanged.emit(this.ingredients.slice())
    }
 
 }
